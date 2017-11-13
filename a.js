3ab440f10b8a9074667ace9f46ed1d4d7576887b
DrB <rjb@rjblaptop.lan>
2017-11-13 05:57:58 +0000

    Modiied to use animationstart event so a lucky guess at the timeout is no longer necessary

--- a.js
+++ a.js
@@ -1,24 +1,9 @@
-var starwars = {
+window.addEventListener("load", () => {
 
-  audio: null,
+  // Normally, be quiet, but, if the URL has a fragment add the music.
+  if (!!window.location.hash) {
+    const audio = new Audio('http://goo.gl/WppRG9');
+    window.logo.addEventListener( "animationstart", () => audio.play() );
+  }
 
-  togglePause: function() {
-    if (starwars.audio.paused) {
-      starwars.audio.play();
-    } else {
-      starwars.audio.pause();
-    }
-	},
-
-	music: function() {
-    starwars.audio = new Audio('http://bit.ly/2fSoYxC');
-    starwars.audio.play();
-	},
-
-	start: function() {
-    setTimeout(starwars.music, 6000);
-	}
-
-};
-
-window.addEventListener("load", starwars.start);
+});
